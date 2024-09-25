8aeb0b4adc2ae1c6adbacd8225f2962db5afb919
jeffrysilva <silvajeffry88@gmail.com>
2024-09-25 11:28:17 +0800

    Simple Implementation, Display via for loop, Multidimensional Array

--- src/it20c/It20c.java
+++ src/it20c/It20c.java
@@ -1,23 +1,12 @@
-
 package it20c;
 
 public class It20c {
   
     public static void main(String[] args) {
-        //this is a nested Array
-        int[][] sampleMatrix = {
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
-        };
-        //for loop intended to get the value of array in a row
-       for(int i = 0;i<sampleMatrix.length;i++){
-           //for loop intended to get the value of array in a column
-        for(int j = 0; j<sampleMatrix[i].length;j++) {
-            //Print the value of the Nested array 
-        System.out.print(sampleMatrix[i][j]+" ");
-        }
-         System.out.println();
-    }           
+        // Create an instance of the devarrays class
+        devarrays myArray = new devarrays();
+        
+        // Call the method to print the array
+        myArray.printArray();
     }   
 }
